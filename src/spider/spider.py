d6e3f9b567e39747cd109b4605d0348d5d41d0b4
南图 <vinthony@gmail.com>
2015-04-05 01:57:53 +0800

    2015年04月05日01:57:46

--- src/spider/spider.py
+++ src/spider/spider.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import requests,functools,
-
+import requests,functools
+from lxml import etree
+import io,sys
 # from utils import log
-def _reg_exp(path):
-	pass;
+def downloader(func_name,body):
+	with io.open(func_name+".rac",'w') as file:
+		file.write(body)
 
+def getFuncName(func):
+	return str(func).split(" ")[1]
+def stardard_url(path):
+	if path.startswith("http://") or path.startswith("https://"):
+		print path
+		return path
+	else:
+		return "http://"+path
+		
 def fetch(path):
 	def _decorator(func):
 		@functools.wraps(func)
 		def _wrapper(*args,**kw):
-			headers = requests.get(path).headers
-			body = requests.get(path).text.encode('UTF-8')
-			return func(headers,body,*args,**kw)
+			#错误处理
+			#download the page
+			r = requests.get(stardard_url(path));
+			headers = r.headers
+			if r.status_code > 400:
+				raise Exception("network connection failed") 
+			body = r.text
+			downloader(getFuncName(func),body)
+			return func(body,*args,**kw)
 		return _wrapper
 	return _decorator		
 
 if __name__ == "__main__":
-
-	@fetch("http://www.baidu.com")
-	def test(headers,body):
-		print headers
-		print body
+	#如果 http://[u+].baidu.com
+	@fetch("www.baidu.com")
+	def test(body):
+		pass
+		#print body
 
 	test()	
