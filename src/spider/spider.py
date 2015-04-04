db8bcc2b424aa3cbc4ac2e2a3be2adef7587fae5
南图 <vinthony@gmail.com>
2015-04-05 00:31:33 +0800

    fixed requests

--- src/spider/spider.py
+++ src/spider/spider.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import requests,functools
+import requests,functools,
+
 # from utils import log
 def _reg_exp(path):
 	pass;
@@ -9,14 +10,17 @@ def fetch(path):
 	def _decorator(func):
 		@functools.wraps(func)
 		def _wrapper(*args,**kw):
-			print requests.get(path).text.encode()
-			return func(*args,**kw)
+			headers = requests.get(path).headers
+			body = requests.get(path).text.encode('UTF-8')
+			return func(headers,body,*args,**kw)
 		return _wrapper
 	return _decorator		
 
 if __name__ == "__main__":
 
-	@fetch("http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/")
-	def test():
-		return 1
+	@fetch("http://www.baidu.com")
+	def test(headers,body):
+		print headers
+		print body
+
 	test()	
