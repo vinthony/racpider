4af55b1df6c7b1aa9ea6436a18fa2670cfacc275
南图 <vinthony@gmail.com>
2015-05-04 00:28:42 +0800

    update redis key

--- src/server/server.py
+++ src/server/server.py
@@ -7,12 +7,8 @@ from redisQueue import RedisQueue
 # - config the redis
 
 r = RedisQueue("rac",host="localhost",port=6379,db=0)
-r.enqueue("http://www.jandan.net")
 
-# - 
-#q = queue.Queue()
-#seeds
-#q.put("http://www.jandan.net")
+r.enqueue("http://www.jandan.net")
 
 
 wsgi = WSGIApplication(os.path.dirname(os.path.abspath(__file__)))
