0971638c1d92586da538f6a62ea19f9b7ff975e5
vinthony <330681740@qq.com>
2015-05-03 17:06:46 +0800

    modify slaver

--- src/slaver/slaver.py
+++ src/slaver/slaver.py
@@ -1,14 +1,23 @@
+#!/usr/bin/env python
 import requests
 import sys
-sys.path.append("..")
-from spider.geturlsfromlinks import getlinks 
+sys.path.append("/home/nantu/dev/racpider/src")
+from spider.geturlsfromlink import getlinks 
 # config server ip:port
-r = requests.get(server.url)
-if r.status_code != 200:
-	log.warning("error:"+str(r.status_code)+":"+str(self.url))
-#doing the spider job
-# url in body?
-links = getlinks(r.text)
+def notempty():
+	e = requests.get("http://192.168.31.110:5237/empty")
+	if int(e.text) > 0 and r.status_code == 200:
+		return True
+	return False
 
-
+while notempty():
+	r = requests.get("http://192.168.31.110:5237/pull")
+	if r.status_code != 200:
+		print "error"
+	links = getlinks(r.text)
+#print links
+	files = {'file':",".join(links)}
+	r2 = requests.post("http://192.168.31.110:5237/push",headers=files)
+	if r2.status_code != 200:
+		break
 
